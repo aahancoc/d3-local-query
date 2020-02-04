5820698203e4dc5dd8c17d1aaa5171a9e74b8137
Allison Hancock <aahancoc@umich.edu>
2020-02-04 13:01:53 -0500

    Continued testing and bugfixing

--- index.js
+++ index.js
@@ -3,40 +3,43 @@ import ShuntingYard, {Operator} from "shunting-yard.js";
 // Currently very dumb, only supports a single data source
 export default function query(query, data)
 {
-	const tokens = tokenize(tokens);
+	const tokens = tokenize(query);
 
 	data = filter_cols(tokens['cols'], data);
 	data = filter_cond(tokens['cond'], data);
 	return data;
 }
 
-function tokenize(query)
+export function tokenize(query)
 {
 	// SELECT <cols> FROM <who cares> WHERE <condition> AND/OR <condition>
 	let idx_select = query.search("SELECT ");
-	let idx_select_end = idx_select + "SELECT ".length();
+	let idx_select_end = idx_select + "SELECT ".length;
 	let idx_from = query.search("FROM ");
-	let idx_from_end = idx_select + "FROM ".length();
+	let idx_from_end = idx_from + "FROM ".length;
 	let idx_where = query.search("WHERE ");
-	let idx_where_end = idx_select + "WHERE ".length();
+	let idx_where_end = idx_where + "WHERE ".length;
 
-	if (!idx_from) {
-		idx_from = idx_select;
-		idx_from_end = idx_select_end;
+	if (idx_from === -1) {
+		return {
+			cols: query.slice(idx_select_end, idx_where).split(',').map(x => x.trim()),
+			srcs: [],
+			cond: query.slice(idx_where_end).trim(),
+		}
+	} else {
+		return {
+			cols: query.slice(idx_select_end, idx_from).split(',').map(x => x.trim()),
+			srcs: query.slice(idx_from_end, idx_where).split(',').map(x => x.trim()),
+			cond: query.slice(idx_where_end).trim(),
+		}
 	}
 
-	return {
-		cols: query.slice(idx_select_end, idx_from).split(',').map(x => x.trim()),
-		srcs: query.slice(idx_from_end, idx_where).split(',').map(x => x.trim()),
-		cond: query.slice(idx_where_end).map(x => x.trim()),
-	}
 }
 
-function filter_cols(cols, data)
+export function filter_cols(cols, data)
 {
-	if (cols === ['*']) {
-		return data;
-	}
+	// Handle '*' special case
+	if (cols.includes('*')) { return data; }
 
 	let output = []
 	data.forEach(d => {
@@ -47,15 +50,21 @@ function filter_cols(cols, data)
 	return output;
 }
 
-function filter_cond(cond, data)
+export function filter_cond(cond, data)
 {
 	// AND has greater precendence than OR
 	const sy = new ShuntingYard();
 	let i = 0;
 	sy.addOperator('AND', new Operator('AND', 2, 'left', 2, (a, b) => a && b));
 	sy.addOperator('OR', new Operator('OR', 1, 'left', 2, (a, b) => a || b));
-	sy.addOperator('=', new Operator('=', 0, 'left', 2, (col, val) => data[i][col] === val));
-	//sy.addOperator('IN', new Operator('IN', 2, (val, col) => data[i][col].test(val)));
+
+	// Operators
+	sy.addOperator(
+		'=', new Operator('=', 0, 'left', 2, (a, b) => {
+			if (Object.keys(data[i]).includes(a)) { return data[i][a] === b; }
+			else { return a === b; }
+		})
+	);
 
 	return data.filter( (d, i_local) => {i = i_local; return !!sy.resolve(cond);});
 }
--- test/test.js
+++ test/test.js
@@ -1,12 +1,110 @@
 import {csvParse} from "d3-dsv";
-import {assert} from "chai"
+import {expect} from "chai"
 import fs from "fs";
-import d3_lq from "../index.js";
+import query, * as d3_lq from "../index.js";
 
-const df = csvParse(fs.readFileSync('./test/test.csv', 'UTF-8'));
+
+describe('tokenize', function() {
+	it('Handles statements with SELECT, FROM, and WHERE', function() {
+		const tests = new Map([
+			[
+				"SELECT * FROM _ WHERE true",
+				{ cols: ['*'], srcs: ['_'], cond: 'true' }
+			], [
+				"SELECT aa, bx, c FROM 1, two WHERE a == 3",
+				{ cols: ['aa', 'bx', 'c'], srcs: ['1', 'two'], cond: 'a == 3' }
+			],
+		]);
+		for (let [query, expected] of tests) {
+			expect(d3_lq.tokenize(query)).to.deep.equal(expected);
+		}
+	});
+
+	it('Handles statements with only SELECT and WHERE', function() {
+		const tests = new Map([
+			[
+				"SELECT * WHERE true",
+				{ cols: ['*'], srcs: [], cond: 'true' }
+			], [
+				"SELECT aa, bx, c WHERE a == 3",
+				{ cols: ['aa', 'bx', 'c'], srcs: [], cond: 'a == 3' }
+			],
+		]);
+		for (let [query, expected] of tests) {
+			expect(d3_lq.tokenize(query)).to.deep.equal(expected);
+		}
+	});
+});
+
+describe('filter_cols', function() {
+	it('Pass through data unchanged on `SELECT *`', function() {
+		const df = [{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 1, 'c': 0}];
+		const tests = new Map([
+			[
+				{ cols: ['*'], data: df },
+				[{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 1, 'c': 0}]
+			]
+		]);
+		for (let [input, expected] of tests) {
+			expect(d3_lq.filter_cols(input.cols, input.data))
+			.to.deep.equal(expected);
+		}
+	});
+
+	it('Selects specific columns', function() {
+		const df = [{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 1, 'c': 0}];
+		const tests = new Map([
+			[
+				{ cols: ['a'], data: df },
+				[{'a': 1}, {'a': 2}]
+			], [
+				{ cols: ['b', 'c'], data: df },
+				[{'b': 2, 'c': 3}, {'b': 1, 'c': 0}]
+			]
+		]);
+		for (let [input, expected] of tests) {
+			expect(d3_lq.filter_cols(input.cols, input.data))
+			.to.deep.equal(expected);
+		}
+	});
+});
+
+describe('filter_cond', function() {
+	it('Pass through data unchanged on `1 = 1`', function() {
+		const df = [{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 1, 'c': 0}];
+		const tests = new Map([
+			[
+				{ cond: '1 = 1', data: df },
+				[{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 1, 'c': 0}]
+			]
+		]);
+		for (let [input, expected] of tests) {
+			expect(d3_lq.filter_cond(input.cond, input.data))
+			.to.deep.equal(expected);
+		}
+	});
+
+	it('Selects rows using `=` statements', function() {
+		const df = [{'a': 1, 'b': 2, 'c': 3}, {'a': 2, 'b': 1, 'c': 0}];
+		const tests = new Map([
+			[
+				{ cond: 'a = 1', data: df },
+				[{'a': 1, 'b': 2, 'c': 3}]
+			], [
+				{ cond: 'c = 0', data: df },
+				[{'a': 2, 'b': 1, 'c': 0}]
+			],
+		]);
+		for (let [input, expected] of tests) {
+			expect(d3_lq.filter_cond(input.cond, input.data))
+			.to.deep.equal(expected);
+		}
+	});
+});
 
 describe('query', function() {
 	it("Passes through data unaltered if requested", function() {
-		assert.equal(d3_lq("SELECT * FROM _ WHERE true", df), df);
+		const df = csvParse(fs.readFileSync('./test/test.csv', 'UTF-8'));
+		expect(query("SELECT * WHERE 1 = 1", df)).to.deep.equal(df);
 	});
 });
